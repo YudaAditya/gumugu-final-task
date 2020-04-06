6e9fd192405d56ebcdd2967ef88daefc98c8f2bc
Yuda Aditya <yudaaditya98@gmail.com>
2020-04-06 16:13:55 +0700

    chart

--- application/views/_layouts/_script.php
+++ application/views/_layouts/_script.php
@@ -2,3 +2,6 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="<?php echo base_url('theme/bootstrap/js/bootstrap.js') ?>"></script>
 <script src="<?php echo base_url('theme/bootstrap/js/bootstrap.bundle.js') ?>"></script>
+<script src="<?php echo base_url('theme/chart/Chart.js') ?>"></script>
+<script src="<?php echo base_url('theme/custom/js/bar-chart.js') ?>"></script>
+<script src="<?php echo base_url('theme/custom/js/pie-chart.js') ?>"></script>
